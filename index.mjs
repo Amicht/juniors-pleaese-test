ae6477b9ae961093ace7f6598267d7a507ecc275
Amicht <vicolicht@gmail.com>
2023-10-02 00:35:16 +0300

    with wild-card path

--- index.mjs
+++ index.mjs
@@ -1,9 +1,19 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = express();
 const PORT = 3001;
 
 server.use(express.static('public'));
+server.get("*", async (req,res)=>{
+    
+    const filePathName = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(filePathName);
+})
 
 
 server.listen(PORT, () => {console.log(`Listening to  http://localhost:${PORT}`)})
